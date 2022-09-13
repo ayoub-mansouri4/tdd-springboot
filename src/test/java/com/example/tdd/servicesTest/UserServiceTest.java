ff3106510656c66b32f012d00ca638e807da7bc2
ayoub-mansouri4 <ayoubmns11@gmail.com>
2022-09-13 16:03:00 +0100

    commit nbr2

--- src/test/java/com/example/tdd/servicesTest/UserServiceTest.java
+++ src/test/java/com/example/tdd/servicesTest/UserServiceTest.java
@@ -4,6 +4,8 @@ import com.example.tdd.services.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -16,7 +18,7 @@ class UserServiceTest {
         underTest = new UserServiceImpl();
     }
 
-    @Test
+    /*@Test
     @DisplayName("numberPhoneValidatorTest is passed successfully")
     void numberPhoneValidatorTest(){
         //Given
@@ -25,6 +27,22 @@ class UserServiceTest {
         boolean isValid =underTest.test(phoneNumber);
         //then
         assertThat(isValid).isTrue();
+
+    }
+     */
+
+     @ParameterizedTest
+     @CsvSource({
+             "+212632236881,true",
+             "212632236881,false",
+             "+3363236881,false",
+             "+212632236881111,false"
+     })
+    void numberPhoneValidatorTest(String phoneNumber,boolean expexted){
+        //when
+        boolean isValid =underTest.test(phoneNumber);
+        //then
+        assertThat(isValid).isEqualTo(expexted);
 
     }
 }
